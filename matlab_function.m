819c68b3137571515c29c9edf8603bcc4108d854
read3636 <133726986+read3636@users.noreply.github.com>
2023-06-06 23:41:28 +0900

    HW13 function 추가

--- matlab_function.m
+++ matlab_function.m
@@ -9,7 +9,7 @@ RAAN = input('Right Ascension of the Ascending Node (degree): ');
 disp(PQW2ECI(arg_prg,inc_angle,RAAN));
 %}
 
-%% HW12
+%{
 semimajor_axis=input('semimajor_axis (Km): ');
 eccentricity=input('ecentricity : ');
 true_anomaly=input('true_anomaly (degree): ');
@@ -18,7 +18,24 @@ disp('rangeInPQW =');
 disp(solveRangeInPerifocalFrame(semimajor_axis,eccentricity,true_anomaly));
 disp('velocityInPQW =');
 disp(solveVelocityInPerifocalFrame(semimajor_axis,eccentricity,true_anomaly));
+%}
 
+%% HW12
+
+YYYY = input('YYYY: ');
+MM = input('MM: ');
+DD = input('DD: ');
+hh = input('hh: ');
+mm = input('mm: ');
+ss = input('ss: ');
+time = datetime(YYYY, MM, DD, hh, mm, ss);
+
+a=[1000,2000,3000;
+    4000,4000,4000;];
+
+disp(ECI2ECEF_DCM(time));
+disp(azimuth(a));
+disp(elevation(a,1));
 
 
 %% HW11
@@ -54,3 +71,33 @@ GM = 3.986004418*10^5; % μ = 3.986004418 × 10^14 [m^3 s^(−2)] --> μ = 3.986
 velocityInPQW=sqrt(GM/p)*[-sin(true_anomaly); eccentricity+cos(true_anomaly); 0;];
 
 end
+
+%% HW13
+function [DCM]=ECI2ECEF_DCM(time)
+
+time=datetime(time);
+jd= juliandate(time);
+GMST = mod(280.4606 + 360.9856473*(jd - 2451545), 360);
+GMST_rad = deg2rad(GMST);
+
+    DCM = [cos(GMST_rad), sin(GMST_rad), 0;
+           -sin(GMST_rad), cos(GMST_rad), 0;
+           0, 0, 1];
+end
+
+function az = azimuth(ENU)
+
+    az = rad2deg(acos(ENU(:,2)./sqrt(ENU(:,1).^2 + ENU(:,3).^2)));
+    az = mod(az, 360);
+end
+
+function el = elevation(ENU, el_mask)
+
+    el = rad2deg(asin(ENU(:,3)./sqrt(ENU(:,1).^2 + ENU(:,2).^2 + ENU(:,3).^2)));
+    
+    el(el < el_mask) = NaN;
+end
+
+
+
+
